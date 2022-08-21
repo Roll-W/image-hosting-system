d83c5ef83f1081c5076e7c6effe770b054208e23
RollW <rollw@lingu.space>
2022-08-21 15:07:04 +0800

    initial.

---
+++ src/test/java/space/lingu/imagehosting/ImageHostingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package space.lingu.imagehosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageHostingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
